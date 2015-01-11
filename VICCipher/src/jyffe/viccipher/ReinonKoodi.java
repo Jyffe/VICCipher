6e34f63e22b1052d54854d77cd90d2621e5b97b9
Jyrki Rantonen <jyrki.rantonen@gmail.com>
2015-01-11 20:04:33 +0200

    Initial commit of the empty project

---
+++ VICCipher/src/jyffe/viccipher/ReinonKoodi.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package jyffe.viccipher;
+
+/**
+ * @author Jyffe
+ *
+ */
+public class ReinonKoodi {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
