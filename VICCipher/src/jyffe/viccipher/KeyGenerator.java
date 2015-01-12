7517da46c873cd9861a3b8e860a3cc864ccf5104
Jyrki Rantonen <jyrki.rantonen@gmail.com>
2015-01-12 11:42:47 +0200

    Added generateKeyG() method to KeyGenerator class and corresponding JUnit test cases

--- VICCipher/src/jyffe/viccipher/KeyGenerator.java
+++ VICCipher/src/jyffe/viccipher/KeyGenerator.java
@@ -5,18 +5,24 @@ package jyffe.viccipher;
 
 /**
  * @author Jyffe
- *
+ * 
+ * Used to generate various keys necessary for VIC-cipher encoding and decoding
  */
 public class KeyGenerator {
 
 	private Sequencer Sequencer = new Sequencer();
+	private ChainCalculator CC = new ChainCalculator();
+	private Mod10Calculator Mod10 = new Mod10Calculator();
+	
 	
 	/**
-	 * @param s
-	 * @return
-	 * @throws Exception
+	 * Generates key S by sequencing String s
+	 * 
+	 * @param s				String seed for generating the key S. Note that s.length() must be exactly 10.
+	 * @return				String representation of key S
+	 * @throws Exception	Thrown if s.length() != 10. Exception message contains reason for the exception.
 	 */
-	public String generateKeyS(String s) throws Exception{
+	public String generateKeyS(String s) throws Exception {
 
 		s = Sequencer.sequence(s);
 		
@@ -29,5 +35,49 @@ public class KeyGenerator {
 			throw new Exception("Sequencer.generateKeyS() : s.length() > 10");
 		}
 		return s;
+	}
+	
+	/**
+	 * Generates key G from date, RI and S1 by using mod10 subtraction, chain addition and mod10 addition.
+	 * 
+	 * <pre>
+	 * TODO:	Check whether date string is valid
+	 * </pre>
+	 * 
+	 * @param date			String representation of a date as a seed for generating key G. Date must be in format dd.mm.yyyy.
+	 * @param RI			Random Indicator String. Ri.length() must be 5.
+	 * @param S1			String representation of the key S1. S1.length() must be 10.
+	 * @return				String representation of key G.
+	 * @throws Exception	Thrown if any of the given seed parameters are not valid. Exception message contains reason for the exception.
+	 */
+	public String generateKeyG(String date, String RI, String S1) throws Exception {
+		
+		String G = null;
+		
+		if(RI.length() < 5){
+			throw new Exception("Sequencer.generateKeyG() : RI.length() < 5");
+		}else if (RI.length() > 5){
+			throw new Exception("Sequencer.generateKeyG() : RI.length() > 5");
+		}
+		
+		if(S1.length() < 10){
+			throw new Exception("Sequencer.generateKeyG() : S1.length() < 10");
+		}else if (S1.length() > 10){
+			throw new Exception("Sequencer.generateKeyG() : S1.length() > 10");
+		}
+		
+		// Remove separators from the date String
+		date = date.replaceAll("\\.+", "");
+
+		// Use and RI five first numbers of the date String for mod10 subtraction to get first iteration of the key G
+		G = Mod10.subtract(RI, date.substring(0, 5));
+		
+		// Chain add 5 iterations to get second iteration of the key G
+		G = CC.add(G, 5);
+		
+		// Use second iteration of the key G and key S1 to get third and final iteration of the key G
+		G = Mod10.add(G, S1);
+		
+		return G;
 	}
 }
--- VICCipher/test/jyffe/viccipher/TestKeyGenerator.java
+++ VICCipher/test/jyffe/viccipher/TestKeyGenerator.java
@@ -21,8 +21,13 @@ public class TestKeyGenerator {
 	public static void tearDownAfterClass() throws Exception {
 	}
 
+	/**
+	 *  Tests for generateKeyS()...
+	 * 
+	 */
+
 	@Test
-	public void testOk() throws Exception {
+	public void testGenerateKeySOk() throws Exception {
 		KeyGenerator tester = new KeyGenerator();
 		
 		assertEquals("6203189574", tester.generateKeyS("IDREAMOFJE"));
@@ -31,7 +36,7 @@ public class TestKeyGenerator {
 	}
 	
 	@Test
-	public void testExceptionStringTooLong() throws Exception {
+	public void testGenerateKeySExceptionStringTooLong() throws Exception {
 		KeyGenerator tester = new KeyGenerator();
 		
 		try{
@@ -44,7 +49,7 @@ public class TestKeyGenerator {
 	}
 	
 	@Test
-	public void testExceptionStringTooShort() throws Exception {
+	public void testGenerateKeySExceptionStringTooShort() throws Exception {
 		KeyGenerator tester = new KeyGenerator();
 		
 		try{
@@ -55,5 +60,69 @@ public class TestKeyGenerator {
 			}
 		}
 	}
+	
+	/** 
+	 * Tests for generateKeyG()...
+	 * 
+	 */
 
+	@Test
+	public void testGenerateGOk() throws Exception {
+		KeyGenerator tester = new KeyGenerator();
+		
+		assertEquals("6551517891", tester.generateKeyG("7.4.1776", "77651", "6203189574"));
+		
+	}
+	
+	@Test
+	public void testGenerateKeyGExceptionRITooLong() throws Exception {
+		KeyGenerator tester = new KeyGenerator();
+		
+		try{
+			tester.generateKeyG("7.4.1776", "776519", "6203189574");
+		} catch (Exception e){
+			if(e.getMessage().compareTo("Sequencer.generateKeyG() : RI.length() > 5") != 0){
+				fail("expected <Sequencer.generateKeyG() : RI.length() > 5>"+" but was <:"+e.getMessage()+">");
+			}
+		}
+	}
+	
+	@Test
+	public void testGenerateKeyGExceptionRITooShort() throws Exception {
+		KeyGenerator tester = new KeyGenerator();
+		
+		try{
+			tester.generateKeyG("7.4.1776", "7765", "6203189574");
+		} catch (Exception e){
+			if(e.getMessage().compareTo("Sequencer.generateKeyG() : RI.length() < 5") != 0){
+				fail("expected <Sequencer.generateKeyG() : RI.length() < 5>"+" but was <:"+e.getMessage()+">");
+			}
+		}
+	}
+	
+	@Test
+	public void testGenerateKeyGExceptionS1TooLong() throws Exception {
+		KeyGenerator tester = new KeyGenerator();
+		
+		try{
+			tester.generateKeyG("7.4.1776", "77651", "62031895741");
+		} catch (Exception e){
+			if(e.getMessage().compareTo("Sequencer.generateKeyG() : S1.length() > 10") != 0){
+				fail("expected <Sequencer.generateKeyG() : S1.length() > 10>"+" but was <:"+e.getMessage()+">");
+			}
+		}
+	}
+	
+	@Test
+	public void testGenerateKeyGExceptionS1TooShort() throws Exception {
+		KeyGenerator tester = new KeyGenerator();
+		
+		try{
+			tester.generateKeyG("7.4.1776", "77651", "620318957");
+		} catch (Exception e){
+			if(e.getMessage().compareTo("Sequencer.generateKeyG() : S1.length() < 10") != 0){
+				fail("expected <Sequencer.generateKeyG() : S1.length() < 10>"+" but was <:"+e.getMessage()+">");
+			}
+		}
+	}
 }
